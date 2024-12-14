404e99ec1457da3dfc3d81b1d9a44626c9d16f9d
candolfi <jcandolfi32@gmail.com>
2024-12-13 19:42:18 -0800

    Modified formatting on Post new.html.erb to add a logout button. Added link to new post on index page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-    before_action :authenticate_user!
+    before_action :authenticate_user!, only: [:new, :create]
 
   def new
     @post = Post.new
@@ -15,24 +15,17 @@ class PostsController < ApplicationController
     if @post.save
         redirect_to root_path
     else
-        render :new, status: :unprocessable_entity
+        render :root_path, status: :unprocessable_entity
     end
   end
 
   def index
+    @posts = Post.all
   end
 
   private
 
-#   def require_login
-#     if user_signed_in?
-#       redirect_to root_path
-#     else
-#         redirect_to root_path
-#     end
-#   end
-
     def post_params
-        params.require(:post).permit(:title, :content, :author)
+        params.require(:post).permit(:content)
     end
 end
